c94dd86e2298786d2285163b15434ed981ec882f
Shubham Hirpara <shubhamhirpara@shubhams-mbp.lan>
2024-01-20 12:30:04 -0500

    first commit

---
+++ src/test/java/co/pragra/learning/candidate/CandidateApplicationTests.java
@@ -0,0 +1,13 @@
+package co.pragra.learning.candidate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
